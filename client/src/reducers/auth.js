c22088e3a0eacd95d33c6a621e56f8fd3176dc42
Asher <asher_3@hotmail.co.uk>
2020-10-14 10:43:01 +0100

    created further authentication for user login with Navbar Links changing based on whether a user is logged in or not

--- client/src/reducers/auth.js
+++ client/src/reducers/auth.js
@@ -1,4 +1,12 @@
-import { REGISTER_SUCCESS, REGISTER_FAIL, AUTH_ERROR, USER_LOADED } from "../actions/types";
+import {
+  REGISTER_SUCCESS,
+  REGISTER_FAIL,
+  AUTH_ERROR,
+  USER_LOADED,
+  LOGIN_SUCCESS,
+  LOGIN_FAIL,
+  LOGOUT,
+} from "../actions/types";
 
 const initialState = {
   token: localStorage.getItem("token"),
@@ -10,14 +18,15 @@ const initialState = {
 export default function (state = initialState, action) {
   const { type, payload } = action;
   switch (type) {
-case USER_LOADED:
-  return {
-    ...state,
-    isAuthenticed: true,
-    loading: false,
-    user: payload
-  }
+    case USER_LOADED:
+      return {
+        ...state,
+        isAuthenticed: true,
+        loading: false,
+        user: payload,
+      };
     case REGISTER_SUCCESS:
+    case LOGIN_SUCCESS:
       localStorage.setItem("token", payload.token);
       return {
         ...state,
@@ -25,8 +34,10 @@ case USER_LOADED:
         isAuthenticated: true,
         loading: false,
       };
-      case AUTH_ERROR:
     case REGISTER_FAIL:
+    case AUTH_ERROR:
+    case LOGIN_FAIL:
+    case LOGOUT:
       localStorage.removeItem("token");
       return {
         ...state,
