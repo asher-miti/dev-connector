15d54c76f1fc3ea310d33c8db27baecf37ffec11
Asher <asher_3@hotmail.co.uk>
2020-10-16 14:43:38 +0100

    added profile management functions for creating and editing a users profile

--- client/src/App.js
+++ client/src/App.js
@@ -6,22 +6,24 @@ import Landing from "./components/layout/Landing";
 import Login from "./components/auth/Login";
 import Register from "./components/auth/Register";
 import Alert from "./components/layout/Alert";
+import Dashboard from "./components/dashboard/Dashboard";
+import CreateProfile from "./components/profile-forms/CreateProfile";
+import PrivateRoute from "./components/routing/PrivateRoute";
 
 // Redux
 import { Provider } from "react-redux";
 import store from "./store";
-import { loadUser } from './actions/auth';
-import setAuthToken from './utils/setAuthToken'
+import { loadUser } from "./actions/auth";
+import setAuthToken from "./utils/setAuthToken";
 
-if(localStorage.token) {
-  setAuthToken(localStorage.token)
+if (localStorage.token) {
+  setAuthToken(localStorage.token);
 }
 
 function App() {
-
   useEffect(() => {
-    store.dispatch(loadUser())
-  }, [])
+    store.dispatch(loadUser());
+  }, []);
 
   return (
     <Provider store={store}>
@@ -34,6 +36,8 @@ function App() {
             <Switch>
               <Route exact path="/register" component={Register} />
               <Route exact path="/login" component={Login} />
+              <PrivateRoute exact path="/dashboard" component={Dashboard} />
+              <PrivateRoute exact path="/create-profile" component={CreateProfile} />
             </Switch>
           </section>
         </Fragment>
