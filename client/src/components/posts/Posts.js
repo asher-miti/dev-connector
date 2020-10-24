6dace267f0909cf236bb65a74991b91c2d413004
Asher <asher_3@hotmail.co.uk>
2020-10-24 10:08:46 +0100

    working on routes for deleting and adding a post

--- client/src/components/posts/Posts.js
+++ client/src/components/posts/Posts.js
@@ -4,6 +4,7 @@ import { connect } from "react-redux";
 import Spinner from "../layout/Spinner";
 import PostItem from "./PostItem";
 import { getPosts } from "../../actions/post";
+import PostForm from "./PostForm";
 
 const Posts = ({ getPosts, post: { posts, loading } }) => {
   useEffect(() => {
@@ -14,11 +15,12 @@ const Posts = ({ getPosts, post: { posts, loading } }) => {
     <Spinner />
   ) : (
     <Fragment>
+      {" "}
       <h1 className="large text-primary"></h1>
       <p className="lead">
         <i className="fas fa-user"></i> Welcome to the community!
       </p>
-      {/* Post form */}
+      <PostForm />
       <div className="posts">
         {posts.map((post) => (
           <PostItem key={post._id} post={post} />
